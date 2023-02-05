2633d80e93caa9f8bf03b04bef03025bad59941a
Nick Blust <75218083+NickBlust@users.noreply.github.com>
2023-02-05 10:57:20 +0100

    implemented win-lose-screen

---
+++ Assets/Scripts/Menus/WinLoseScreen.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoseScreen : MonoBehaviour
+{
+    [SerializeField] Canvas winCanvas;
+    [SerializeField] Canvas loseCanvas;
+    [SerializeField] Canvas backgroundCanvas;
+
+    public void OpenWinScreen() 
+    {
+        winCanvas.enabled = true;
+        backgroundCanvas.enabled = true;
+
+    }
+
+    public void OpenLoseScreen() 
+    {
+        loseCanvas.enabled = true;
+        backgroundCanvas.enabled = true;
+    }
+
+    private void CloseWinLoseScreen()
+    {
+        loseCanvas.enabled = false;
+        loseCanvas.enabled = false;
+        backgroundCanvas.enabled = false;
+    }
+
+    public void RetryLevel() 
+    {
+        CloseWinLoseScreen();
+        StartCoroutine(RetryLevelCR());
+    }
+
+    public void GoBackToStart()
+    {
+        CloseWinLoseScreen();
+        StartCoroutine(GoBackToStartCR());
+    }
+
+    private IEnumerator GoBackToStartCR()
+    {
+        CloseWinLoseScreen();
+        yield return TransitionsManager.Instance.PlayOutro();
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    private IEnumerator RetryLevelCR()
+    {
+        yield return TransitionsManager.Instance.PlayOutro();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
